161a6ceae8ef75671ff28e451ccc68c5d51737f7
kandgesid <kandgesid@gmail.com>
2025-08-06 12:42:41 -0700

    handled storing of post stage transition rating survey to DB

--- collect/app.py
+++ collect/app.py
@@ -3,7 +3,7 @@ import yaml # type: ignore
 import boto3 # type: ignore
 import time
 import random
-from focus_utils import CORS_HEADERS, DAILY_SURVEY_DATA_TABLE_NAME, POST_STAGE_SURVEY_DATA_TABLE_NAME, POST_STUDY_SURVEY_DATA_TABLE_NAME, update_last_active_time, update_user_stage, decimal_to_int
+from focus_utils import CORS_HEADERS, DAILY_SURVEY_DATA_TABLE_NAME, POST_STAGE_SURVEY_DATA_TABLE_NAME, POST_STUDY_SURVEY_DATA_TABLE_NAME, POST_STAGE_RATING_SURVEY_DATA_TABLE_NAME, update_last_active_time, update_user_stage, decimal_to_int
 
 def lambda_handler(event, context):
     """Used to collect data for the FocusMode Study
@@ -152,6 +152,7 @@ def lambda_handler(event, context):
             daily_survey_data_table = dynamodb.Table(DAILY_SURVEY_DATA_TABLE_NAME)
             post_stage_survey_data_table = dynamodb.Table(POST_STAGE_SURVEY_DATA_TABLE_NAME)
             post_study_survey_data_table = dynamodb.Table(POST_STUDY_SURVEY_DATA_TABLE_NAME)
+            post_stage_rating_survey_data_table = dynamodb.Table(POST_STAGE_RATING_SURVEY_DATA_TABLE_NAME)
             
             requested_body["Id"] = f"{int(time.time() * 1000)}-{random.randint(1000, 9999)}"
             if data_type == "daily_survey":
@@ -164,6 +165,10 @@ def lambda_handler(event, context):
                 )
             elif data_type == "post_study_survey":
                 post_study_survey_data_table.put_item(
+                    Item=requested_body
+                )
+            elif data_type == "post_stage_rating_survey":
+                post_stage_rating_survey_data_table.put_item(
                     Item=requested_body
                 )
                 
